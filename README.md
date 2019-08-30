596e5941f648e29b52e4a270e0ecea31f38ce8b1
Martin <neo.cortex.bg@gmail.com>
2019-08-31 01:33:31 +0300

    M README.md ?? .gitignore ?? examples/

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # php-debug
+
+Examples
+```
+php -S localhost:8000 -t examples
+```
+
+Open http://localhost:8000
---
+++ examples/index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Examples</title>
+</head>
+
+<body>
+<ul>
+    <li><<a href="example1.php">example1.php</a></li>
+</ul>
+</body>
+
+</html>
